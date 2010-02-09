b50967e80d3a076875de75d6c8fb7ba32f0f4d8a
Eric Davis <edavis@littlestreamsoftware.com>
2010-02-09 09:33:00 -0800

    Generated plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_extra_ldap do
+  name 'Redmine Extra Ldap plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
