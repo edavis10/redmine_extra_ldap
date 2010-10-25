631207a8b0a52092117dc2ca921bd1675970e549
Eric Davis <edavis@littlestreamsoftware.com>
2010-10-25 10:29:21 -0700

    [#4598] Redirect back to the LdapAuthSources#index. New Redmine action.

--- app/controllers/ldap_syncs_controller.rb
+++ app/controllers/ldap_syncs_controller.rb
@@ -8,6 +8,6 @@ class LdapSyncsController < ApplicationController
     ExtraLdap.update_custom_user_data(:all)
     
     flash[:notice] = l(:label_sync_completed)
-    redirect_to :controller => 'auth_sources', :action => 'list'
+    redirect_to :controller => 'ldap_auth_sources', :action => 'index'
   end
 end
